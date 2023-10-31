29ad877af8ffdc3cb461ec428473e73011da6b13
Alyssa Joaquin <alyssa.joaquin@gmail.com>
2023-10-31 13:13:40 -0400

    Added spotify top songs chart

--- spotify.py
+++ spotify.py
@@ -7,7 +7,7 @@ import seaborn as sns
 st.header("Spotify Top Streamed Songs 2023")
 data = pd.read_csv("spotify-2023.csv", encoding='ISO-8859-1')
 data.info()
-
+# top artists with most songs
 top_artists = data['artist(s)_name'].value_counts().head(10)
 
 fig = plt.figure(figsize=(12, 6))
@@ -17,3 +17,11 @@ plt.ylabel('Artist(s) Name')
 plt.title('Top 10 Artists with Most Songs')
 st.pyplot(fig)
 
+#top 10 songs with most streams
+top_spotify_streams = data[['track_name', 'artist(s)_name', 'streams']].sort_values(by='streams', ascending=False).head(10)
+fig2 = plt.figure(figsize=(12, 6))
+sns.barplot(x = top_spotify_streams['streams'], y = top_spotify_streams['track_name'], palette= 'viridis')
+plt.xlabel('Streams (in billions)')
+plt.ylabel('Song Name')
+plt.title('Top 10 Songs with Most Streams on Spotify')
+st.pyplot(fig2)
